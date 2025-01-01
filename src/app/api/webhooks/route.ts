eb60b89be570cb2de627fd702879f27811d0f8e4
Ibrahim doba <ibrahimdoba55@gmail.com>
2025-01-01 19:20:02 +0100

    build fix

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -1,4 +1,4 @@
-// /* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import db from "@/lib/db";
 import { PLANS } from "@prisma/client";
 import crypto from "crypto";
