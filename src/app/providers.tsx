7c85749cb5f21b618fb68230f1375be1f74219ed
Ibrahim doba <ibrahimdoba55@gmail.com>
2025-01-08 00:16:07 +0100

    vercel analytics

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,12 +2,14 @@
 import { SessionProvider } from 'next-auth/react';
 import React from 'react';
 import { Toaster } from '@/components/ui/sonner';
+import { Analytics } from '@vercel/analytics/react';
 
 function Providers({ children }: { children: React.ReactNode }) {
   return (
  <>
       <SessionProvider>{children}</SessionProvider>
       <Toaster />
+      <Analytics/>
     </>
   );
 }
